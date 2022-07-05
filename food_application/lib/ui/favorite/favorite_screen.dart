9352767944b4ab4df072e12bd0baf6bd85ccbcbb
Kadriye <kadriyemacit9655@gmail.com>
2022-07-05 22:54:15 +0300

    Update favorite

--- food_application/lib/ui/favorite/favorite_screen.dart
+++ food_application/lib/ui/favorite/favorite_screen.dart
@@ -18,6 +18,16 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
     "Cheesecake",
   ];
 
+  final List<String> _foodImages = [
+    "assets/bow.png",
+    "assets/fresh.png",
+    "assets/healthy.png",
+    "assets/bow.png",
+    "assets/fresh.png",
+    "assets/healthy.png",
+    "assets/bow.png",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,30 +40,78 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
           ),
           itemCount: _foodNames.length,
           itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Stack(
-                  children: [
-                    const Align(
-                      alignment: Alignment.topRight,
-                      child: Icon(
-                        Icons.favorite,
-                        color: Colors.orange,
+            return InkWell(
+              onTap: () {
+                _showAlertDialog(index);
+              },
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Stack(
+                    children: [
+                      const Align(
+                        alignment: Alignment.topRight,
+                        child: Icon(
+                          Icons.favorite,
+                          color: Colors.orange,
+                        ),
                       ),
-                    ),
-                    Center(
-                      child: Text(
-                        _foodNames[index],
+                      Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              _foodNames[index],
+                            ),
+                            Image.asset(
+                              _foodImages[index],
+                              height: 120,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             );
           },
         ),
       ),
+    );
+  }
+
+  void _showAlertDialog(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(_foodNames[index]),
+          content: const Text("Silmek istediğinize emin misiniz?"),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _foodNames.removeAt(index);
+                });
+                Navigator.pop(context);
+              },
+              child: const Text("Evet"),
+            ),
+            RawMaterialButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                  color: Colors.orange,
+                  child: const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text("Hayır"),
+                  )),
+            )
+          ],
+        );
+      },
     );
   }
 }
